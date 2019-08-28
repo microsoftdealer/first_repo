1b3a69a304b1a1bf01c4961c37045524a0cde451
microsoftdealer <mishagarin@gmail.com>
2019-08-28 06:55:40 -0500

    shit

--- dst_nat_into_vrf.py
+++ dst_nat_into_vrf.py
@@ -1,11 +1,11 @@
 import getpass
-from netmiko import ConnectionHandler
+from netmiko import ConnectHandler
 
 def dst_nat_into_vrf():
-	***
+    """
 	This script made for making dst-nat from main routing table
 	to VRF in KRAUD-DC-ROUTER
-	***
+	"""
 	
     device_params = {
             'device_type': 'mikrotik_routeros',
@@ -27,14 +27,15 @@ def dst_nat_into_vrf():
     #if not outside_port:
     #    outside_port = 'ether2'
     outside_port_dstnat = input('Put outside port for dstnat(Public port): ')
-	inside_port = input('Put destination port(only port):') 
+    inside_port = input('Put destination port(only port):') 
     inside_address = input('Put inside address for dstnat (Inside adress): ')
     commands = []
-	commands.append(f'/ip firewall mangle add action=mark-connection chain=prerouting connection-state=new dst-address={outside_address} dst-port={outside_port_dstnat} in-interface={outside_int} new-connection-mark=into-vrf passthrough=yes protocol=tcp comment="DST_NAT_MANGLE_RULE_BY_SCRIPT FOR LEAKING FROM VRF"')
-    commands.append(f'/ip firewall nat add action=dst-nat chain=dstnat comment="DST_NAT_MANGLE_RULE_BY_SCRIPT FOR LEAKING FROM VRF" dst-address={outside_address} dst-port={outside_port_dstnat} in-interface={outside_int} protocol=tcp to-addresses={inside_address} to-ports={inside_port}')
+    commands.append(f'/ip firewall mangle add action=mark-connection chain=prerouting connection-state=new dst-address={outside_address} dst-port={outside_port_dstnat} in-interface=ether2 new-connection-mark=into-vrf passthrough=yes protocol=tcp comment="DST_NAT_MANGLE_RULE_BY_SCRIPT FOR LEAKING FROM VRF"')
+    commands.append(f'/ip firewall nat add action=dst-nat chain=dstnat comment="DST_NAT_MANGLE_RULE_BY_SCRIPT FOR LEAKING FROM VRF" dst-address={outside_address} dst-port={outside_port_dstnat} in-interface=ether2 protocol=tcp to-addresses={inside_address} to-ports={inside_port}')
     
-    with ConnectionHandler(**device_params) as ssh:
-        ssh.send_commands(commands)
+    with ConnectHandler(**device_params) as ssh:
+        for comm in commands:
+            ssh.send_command(comm)
     return print(f'"{commands[0]}" and "{commands[1]}" are sent to device')
 	
 if __name__ == "__main__":
